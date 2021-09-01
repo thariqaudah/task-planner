2de108b22013cd042bd95c7e12a1d8835cc4890a
Thariq Audah <thariq.audah@gmail.com>
2021-09-01 09:43:40 +0700

    Working on pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,63 +1,80 @@
 <template>
-	<div class="pagination">
-		<button @click="currentPage - 1">
-			<i class="fas fa-arrow-circle-left"></i>
-		</button>
-		<button v-for="page in pages" :key="page">{{ page }}</button>
-		<button @click="currentPage + 1">
-			<i class="fas fa-arrow-circle-right"></i>
-		</button>
-	</div>
+  <div class="pagination">
+    <button @click="handleClick('dec')" v-if="currentPage * 3 > 3">
+      <i class="fas fa-arrow-circle-left"></i>
+    </button>
+    <button
+      v-for="page in pages"
+      :key="page"
+      @click="handleClick(null, $event)"
+      :class="{ active: page === currentPage }"
+    >
+      {{ page }}
+    </button>
+    <button @click="handleClick('inc')" v-if="countDocs > currentPage * 3">
+      <i class="fas fa-arrow-circle-right"></i>
+    </button>
+  </div>
 </template>
 
 <script>
 import { onMounted, ref } from 'vue'
 
 export default {
-	name: 'Pagination',
-	props: ['countDocs'],
-	setup(props, { emit }) {
-		const pages = ref([])
-		const currentPage = ref(1)
-		const limit = 3 // document per page
+  name: 'Pagination',
+  props: ['countDocs', 'currentPage'],
+  setup(props, { emit }) {
+    const pages = ref([])
+    const limit = 3 // limit document per page
 
-		onMounted(async () => {
-			let numberOfPages = Math.ceil(props.countDocs / limit)
-			for (let i = 1; i <= numberOfPages; i++) {
-				pages.value.push(i)
-			}
-		})
+    onMounted(async () => {
+      let numberOfPages = Math.ceil(props.countDocs / limit)
+      for (let i = 1; i <= numberOfPages; i++) {
+        pages.value.push(i)
+      }
+    })
 
-		const handleClick = e => {
-			const page = parseInt(e.target.innerText, 10)
-			emit('paginate', page)
-		}
+    const handleClick = (option, e) => {
+      let page
 
-		return { pages, currentPage, handleClick }
-	},
+      switch (option) {
+        case 'inc':
+          page = props.currentPage + 1
+          break
+        case 'dec':
+          page = props.currentPage - 1
+          break
+        default:
+          page = parseInt(e.target.innerText, 10)
+      }
+      emit('paginate', page)
+    }
+
+    return { pages, handleClick }
+  },
 }
 </script>
 
 <style scoped>
 .pagination {
-	margin: 50px auto;
-	text-align: center;
+  margin: 50px auto;
+  text-align: center;
 }
 .pagination > button {
-	display: inline-block;
-	border: none;
-	outline: none;
-	background: none;
-	color: var(--secondary-color);
-	font-size: 1rem;
-	margin: 0 0.8rem;
+  display: inline-block;
+  border: none;
+  outline: none;
+  background: none;
+  color: var(--secondary-color);
+  font-size: 1rem;
+  margin: 0 0.8rem;
 }
 .pagination button.active {
-	color: var(--primary-color);
-	font-weight: 700;
-	border-bottom: 1px solid var(--primary-color);
+  color: var(--primary-color);
+  font-weight: 700;
+  border-bottom: 1px solid var(--primary-color);
 }
 .pagination button:hover {
-	color: var(--primary-color);
+  color: var(--primary-color);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,32 @@
 <template>
-	<div class="home container">
-		<div v-if="tasks.length">
-			<FilterNav @filter="handleFilter" :filterBy="filterBy" />
-			<div class="layout">
-				<div>
-					<ItemView
-						v-for="task in filteredTasks"
-						:key="task.id"
-						:task="task"
-						@delete="handleDelete"
-					/>
-					<Pagination @paginate="handlePaginate" :countDocs="tasks.length" />
-				</div>
-				<TagUniverse :tasks="tasks" />
-			</div>
-		</div>
-		<div v-else>
-			<h3>Loading...</h3>
-		</div>
-		<div v-if="error">
-			<h3>Error: {{ error }}</h3>
-		</div>
-	</div>
+  <div class="home container">
+    <div v-if="tasks.length">
+      <FilterNav @filter="handleFilter" :filterBy="filterBy" />
+      <div class="layout">
+        <div>
+          <ItemView
+            v-for="task in tasksToDisplay"
+            :key="task.id"
+            :task="task"
+            @delete="handleDelete"
+          />
+          <Pagination
+            :countDocs="tasks.length"
+            :currentPage="currentPage"
+            @paginate="console.log('ok')"
+            v-if="filterBy === 'all'"
+          />
+        </div>
+        <TagUniverse :tasks="tasks" />
+      </div>
+    </div>
+    <div v-else>
+      <h3>Loading...</h3>
+    </div>
+    <div v-if="error">
+      <h3>Error: {{ error }}</h3>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -33,49 +38,60 @@ import Pagination from '../components/Pagination.vue'
 import getCollection from '../composables/getCollection'
 
 export default {
-	name: 'Home',
-	components: { ItemView, TagUniverse, FilterNav, Pagination },
-	setup() {
-		const filterBy = ref('all')
-		const filteredTasks = computed(() => {
-			switch (filterBy.value) {
-				case 'inProgress':
-					return tasks.value.filter(task => !task.done)
-				case 'completed':
-					return tasks.value.filter(task => task.done)
-				default:
-					return tasks.value.filter(task => task)
-			}
-		})
+  name: 'Home',
+  components: { ItemView, TagUniverse, FilterNav, Pagination },
+  setup() {
+    const filterBy = ref('all')
+    const currentPage = ref(1)
 
-		const { docs: tasks, error, loading, getDocs } = getCollection('tasks')
+    const { docs: tasks, error, loading, getDocs } = getCollection('tasks')
 
-		onMounted(async () => {
-			await getDocs()
-		})
+    const filteredTasks = computed(() => {
+      switch (filterBy.value) {
+        case 'inProgress':
+          return tasks.value.filter(task => !task.done)
+        case 'completed':
+          return tasks.value.filter(task => task.done)
+        default:
+          return tasks.value.filter(task => task)
+      }
+    })
 
-		const handleDelete = id => {
-			tasks.value = tasks.value.filter(task => task.id !== id)
-		}
+    const tasksToDisplay = computed(() => {
+      const limit = 3
+      const startIndex = (currentPage.value - 1) * limit
+      const endIndex = currentPage.value * limit
+      return filteredTasks.value.slice(startIndex, endIndex)
+    })
 
-		const handleFilter = option => {
-			filterBy.value = option
-		}
+    onMounted(async () => {
+      await getDocs()
+    })
 
-		const handlePaginate = page => {
-			console.log(page)
-		}
+    const handleDelete = id => {
+      tasks.value = tasks.value.filter(task => task.id !== id)
+    }
 
-		return {
-			tasks,
-			error,
-			loading,
-			filterBy,
-			filteredTasks,
-			handleDelete,
-			handleFilter,
-			handlePaginate,
-		}
-	},
+    const handleFilter = option => {
+      filterBy.value = option
+    }
+
+    const handlePaginate = page => {
+      currentPage.value = page
+    }
+
+    return {
+      tasks,
+      error,
+      loading,
+      filterBy,
+      filteredTasks,
+      currentPage,
+      tasksToDisplay,
+      handleDelete,
+      handleFilter,
+      handlePaginate,
+    }
+  },
 }
 </script>
