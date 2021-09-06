4c653ce3fb19456e11ca7125dfcded91b3518e16
Thariq Audah <thariq.audah@gmail.com>
2021-09-06 14:30:33 +0700

    Hide env vars

--- client/src/composables/getCollection.js
+++ client/src/composables/getCollection.js
@@ -2,40 +2,40 @@ import { ref } from 'vue'
 import useApi from './useApi'
 
 const getCollection = collection => {
-  const docs = ref([])
-  const doc = ref(null)
-  const error = ref(null)
-  const loading = ref(false)
+	const docs = ref([])
+	const doc = ref(null)
+	const error = ref(null)
+	const loading = ref(false)
 
-  const { api } = useApi()
+	const { api } = useApi()
 
-  const getDocs = async () => {
-    try {
-      loading.value = true
-      const res = await api.get(`/${collection}?_sort=-createdAt`)
-      docs.value = res.data.tasks
-      loading.value = false
-      return res
-    } catch (err) {
-      console.log(err)
-      error.value = err.message
-    }
-  }
+	const getDocs = async () => {
+		try {
+			loading.value = true
+			const res = await api.get(`/${collection}`)
+			docs.value = res.data.tasks
+			loading.value = false
+			return res
+		} catch (err) {
+			console.log(err)
+			error.value = err.message
+		}
+	}
 
-  const getDoc = async id => {
-    try {
-      loading.value = true
-      const res = await api.get(`/${collection}/${id}`)
-      doc.value = res.data.task
-      loading.value = false
-      return res.data
-    } catch (err) {
-      console.log(err)
-      error.value = err.message
-    }
-  }
+	const getDoc = async id => {
+		try {
+			loading.value = true
+			const res = await api.get(`/${collection}/${id}`)
+			doc.value = res.data.task
+			loading.value = false
+			return res.data
+		} catch (err) {
+			console.log(err)
+			error.value = err.message
+		}
+	}
 
-  return { docs, doc, error, loading, getDocs, getDoc }
+	return { docs, doc, error, loading, getDocs, getDoc }
 }
 
 export default getCollection
